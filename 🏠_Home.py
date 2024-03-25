113db287bc858287eaf1398476743d5959eab8a9
Seba Gutierrez <sebagm123@gmail.com>
2024-03-25 15:15:15 -0500

    home page

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -7,7 +7,9 @@ st.set_page_config(
 
 st.write("# Visa Manager")
 
-st.write("""
+main_tab, schengen_tab, usa_tab = st.tabs(['App Desc', 'Schengen', 'USA'])
+
+schengen_tab.write("""
     ## Schengen Tourist Visa
     [The Schengen area](https://www.schengenvisainfo.com/schengen-visa-countries-list/) represents a border-free zone 
     between several European countries. In addition, these countries issue a uniform Schengen visa for foreign 
@@ -20,7 +22,8 @@ st.write("""
     Schengen zone (any country within the area) can stay for up to 90 days within **any 180 days**. At first glance, it 
     seems a very simple rule, but it’s often misunderstood, and many people overstay it, resulting in them facing 
     penalties. This is why it’s important to know how exactly the 90/180-day rule works. 
-    [*www.schengenvisainfo.com*](https://www.schengenvisainfo.com/visa-calculator/)
+    [*www.schengenvisainfo.com*](https://www.schengenvisainfo.com/visa-calculator/)""")
 
+usa_tab.write("""
     ## Visa Waiver USA
     something...""")
