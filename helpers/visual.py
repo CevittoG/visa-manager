fe7fd0bc10a78fcf859bd734ceb0412e08a72462
Alfredo Gutierrez <alfredo.gutierrez@u-planner.com>
2024-03-25 14:59:34 -0500

    edit page

---
+++ helpers/visual.py
@@ -0,0 +1,62 @@
+import random
+import streamlit as st
+import numpy as np
+
+
+def show_GIF(gif_type: str, st_structure=st, msg: str = None):
+    working_gifs = ['https://media.giphy.com/media/VekcnHOwOI5So/giphy.gif',
+                    'https://media.giphy.com/media/13rQ7rrTrvZXlm/giphy.gif',
+                    'https://media.giphy.com/media/toXKzaJP3WIgM/giphy.gif',
+                    'https://media.giphy.com/media/5Zesu5VPNGJlm/giphy.gif',
+                    'https://media.giphy.com/media/xiAqCzbB3eZvG/giphy.gif',
+                    'https://media.giphy.com/media/tn3kTJo4P4y1G/giphy.gif']
+    error_gifs = ['https://media.giphy.com/media/3BlItBedmyoAU/giphy.gif',
+                  'https://media.giphy.com/media/AhjXalGPAfJg4/giphy.gif',
+                  'https://media.giphy.com/media/8EmeieJAGjvUI/giphy.gif',
+                  'https://media.giphy.com/media/6AaB96ZVrUN0I/giphy.gif',
+                  'https://media.giphy.com/media/MZocLC5dJprPTcrm65/giphy.gif',
+                  'https://media.giphy.com/media/ljtfkyTD3PIUZaKWRi/giphy.gif',
+                  'https://media.giphy.com/media/0DhHqfExMMT7VdeqIr/giphy.gif']
+    success_gifs = ['https://media.giphy.com/media/tkApIfibjeWt1ufWwj/giphy.gif',
+                    'https://media.giphy.com/media/cXblnKXr2BQOaYnTni/giphy.gif',
+                    'https://media.giphy.com/media/IwAZ6dvvvaTtdI8SD5/giphy.gif',
+                    'https://media.giphy.com/media/4xpB3eE00FfBm/giphy.gif']
+    gifs = {'working': working_gifs,
+            'error': error_gifs,
+            'success': success_gifs}
+
+    msg_type = {'info': '' if msg is None else msg,
+                'working': 'Aún no terminamos de trabajar en esta nueva característica de la plataforma, pero prometemos terminar pronto...' if msg is None else msg,
+                'error': 'Algo ha salido mal 😥, por favor contactar a Data Service para notificar problema.' if msg is None else msg,
+                'success': 'Todo resultó bien!' if msg is None else msg}
+    _, c, _ = st_structure.columns([3, 4, 3])
+    c.image(random.choice(gifs[gif_type]), use_column_width=True)
+
+    if gif_type == 'info':
+        c.info(msg_type[gif_type])
+    elif gif_type == 'working':
+        c.warning(msg_type[gif_type])
+    elif gif_type == 'error':
+        c.error(msg_type[gif_type])
+    elif gif_type == 'success':
+        c.success(msg_type[gif_type])
+
+
+def highlight_column(bgcolor="lightblue", color="black"):
+    return f'background-color: {bgcolor}; color: {color};'
+
+
+def highlight_with_function(value, function, bgcolor="#f9aeae", color="black"):
+    if function(value):
+        return f'background-color: {bgcolor}; color: {color};'
+    else:
+        return ""
+
+
+def highlight_missing_values(value, bgcolor="#f9aeae", color="black"):
+    if type(value) == type("") and value == "":
+        return f'background-color: {bgcolor}; color: {color};'
+    elif type(value) in [type(np.nan)] and np.isnan(value):
+        return f'background-color: {bgcolor}; color: {color};'
+    else:
+        return ""
