289a0df4eb4daf2a39e4b5926359927621069332
Seba Gutierrez <sebagm123@gmail.com>
2024-03-26 19:56:19 -0500

    script structure

--- pages/1_➕_Add_Trip.py
+++ pages/1_➕_Add_Trip.py
@@ -1,41 +1,45 @@
 from helpers import COUNTRIES, TravelHistory, Trip, SCHENGEN_COUNTRIES
-from helpers.streamlit import page_recognition, shared_page_config, TH_DF_CONFIG
+from helpers.streamlit import page_recognition, shared_page_config, TH_DF_CONFIG, DATE_FORMAT
+from helpers.visual import highlight_invalid_trip
 import streamlit as st
 import os
 
-########################################################################################################################
-# INITIAL PAGE SETTING
-########################################################################################################################
-# Recognize page name
-PAGE_EMOJI, PAGE_KEYWORD = page_recognition(os.path.basename(__file__))
 
-# Start with page content
-page_caption = ("On this page you can add trips to your travel history. It will only be necessary to select country and dates, the app will do the rest...")
-shared_page_config(title=PAGE_KEYWORD.replace('_', ' ').title(),
-                   emoji=PAGE_EMOJI,
-                   page_caption=page_caption,)
+# --------------------------------------------------------------------------- PAGE FUNCTIONALITY
+def main():
+    c1, c2, c3, c4 = st.columns([2, 1, 1, 1])
 
-if 'TravelHistory' not in st.session_state:
-    st.session_state['TravelHistory'] = TravelHistory()
+    country = c1.selectbox('Country', SCHENGEN_COUNTRIES)
+    entry_date = c2.date_input('Entry date', format=DATE_FORMAT)
+    exit_date = c3.date_input('Exit date', format=DATE_FORMAT)
+    c4.text("")
+    c4.text("")
+    if c4.button('Add trip', type='primary'):
+        new_trip = Trip(country, entry_date, exit_date)
+        st.session_state['TravelHistory'].add_trip(new_trip)
+        st.toast(f":{'green' if new_trip.valid else 'red'}[{new_trip}]", icon='✅' if new_trip.valid else '⛔')
 
-
-########################################################################################################################
-# PAGE FUNCTIONALITY
-########################################################################################################################
-
-c1, c2, c3, c4 = st.columns([2, 1, 1, 1])
-
-country = c1.selectbox('Country', SCHENGEN_COUNTRIES)
-entry_date = c2.date_input('Entry date')
-exit_date = c3.date_input('Exit date')
-c4.text("")
-c4.text("")
-if c4.button('Add trip', type='primary'):
-    new_trip = Trip(country, entry_date, exit_date)
-    st.session_state['TravelHistory'].add_trip(new_trip)
-    st.write(new_trip)
-
-if not st.session_state['TravelHistory'].to_df().empty:
+    # Show TravelHistory data
     trips_df = st.session_state['TravelHistory'].to_df()
-    trips_df = trips_df[['country', 'entry_date', 'exit_date', 'days', 'limit_date', 'days_left', 'entry_eval_date', 'entry_eval_days', 'exit_eval_date', 'exit_eval_days', 'renew_date']]
-    st.dataframe(trips_df, column_config=TH_DF_CONFIG)
+    if not trips_df.empty:
+        # Mark with red all invalid trips
+        trips_df = trips_df.style.apply(highlight_invalid_trip, axis=1)
+        trips_df = trips_df.hide(['is_valid'], axis=1)
+        st.dataframe(trips_df, column_config=TH_DF_CONFIG)
+
+
+if __name__ == "__main__":
+    # Recognize page name
+    PAGE_EMOJI, PAGE_KEYWORD = page_recognition(os.path.basename(__file__))
+
+    # Start with page content
+    page_caption = ("On this page you can add trips to your travel history. It will only be necessary to select country and dates, the app will do the rest...")
+    shared_page_config(title=PAGE_KEYWORD.replace('_', ' ').title(),
+                       emoji=PAGE_EMOJI,
+                       page_caption=page_caption, )
+
+    if 'TravelHistory' not in st.session_state:
+        st.session_state['TravelHistory'] = TravelHistory()
+
+    # Page functionality
+    main()
--- pages/2_✏️_Edit_Trip.py
+++ pages/2_✏️_Edit_Trip.py
@@ -1,33 +1,38 @@
 from helpers import COUNTRIES, TravelHistory, Trip, SCHENGEN_COUNTRIES
-from helpers.streamlit import page_recognition, shared_page_config
+from helpers.streamlit import page_recognition, shared_page_config, TH_DF_CONFIG, DATE_FORMAT
 import streamlit as st
 import os
 
-########################################################################################################################
-# INITIAL PAGE SETTING
-########################################################################################################################
-# Recognize page name
-PAGE_EMOJI, PAGE_KEYWORD = page_recognition(os.path.basename(__file__))
 
-# Start with page content
-page_caption = (" On this page you can edit records from your travel history. It will only be necessary to select the cell you want to edit in the following table, and then complete a new value. If you wish to remove"
-                "a trip, just select the last column ('Remove') and then save your changes.")
-shared_page_config(title=PAGE_KEYWORD.replace('_', ' ').title(),
-                   emoji=PAGE_EMOJI,
-                   page_caption=page_caption,)
-
-if 'TravelHistory' not in st.session_state:
-    st.session_state['TravelHistory'] = TravelHistory()
-
-
-########################################################################################################################
-# PAGE FUNCTIONALITY
-########################################################################################################################
-if not st.session_state['TravelHistory'].to_df().empty:
+# --------------------------------------------------------------------------- PAGE FUNCTIONALITY
+def main():
+    # Show TravelHistory data
     trips_df = st.session_state['TravelHistory'].to_df()
-    trips_df = trips_df[['type', 'country', 'entry_date', 'exit_date', 'days', 'limit_date', 'days_left']]
-    trips_df['remove'] = False
-    edited_trips_df = st.data_editor(trips_df, column_config=TH_DF_CONFIG)
+    if not trips_df.empty:
+        trips_df = st.session_state['TravelHistory'].to_df()
+        trips_df = trips_df[['type', 'country', 'entry_date', 'exit_date', 'days', 'limit_date', 'days_left']]
+        trips_df['remove'] = False
+        edited_trips_df = st.data_editor(trips_df, column_config=TH_DF_CONFIG)
 
-    if st.button('Save changes', type='primary'):
-        st.session_state['TravelHistory'].update_trips(edited_trips_df)
+        if st.button('Save changes', type='primary'):
+            messages = st.session_state['TravelHistory'].update_trips(edited_trips_df)
+            for msg in messages:
+                st.toast(msg[0], icon=msg[1])
+
+
+if __name__ == "__main__":
+    # Recognize page name
+    PAGE_EMOJI, PAGE_KEYWORD = page_recognition(os.path.basename(__file__))
+
+    # Start with page content
+    page_caption = (" On this page you can edit records from your travel history. It will only be necessary to select the cell you want to edit in the following table, and then complete a new value. If you wish to remove"
+                    "a trip, just select the last column ('Remove') and then save your changes.")
+    shared_page_config(title=PAGE_KEYWORD.replace('_', ' ').title(),
+                       emoji=PAGE_EMOJI,
+                       page_caption=page_caption, )
+
+    if 'TravelHistory' not in st.session_state:
+        st.session_state['TravelHistory'] = TravelHistory()
+
+    # Page functionality
+    main()
