c08bd9993ed0be062156dd4b8d11cc3621981bdd
Seba Gutierrez <sebagm123@gmail.com>
2024-04-01 10:49:42 -0500

    Sing-up

--- helpers/database.py
+++ helpers/database.py
@@ -4,15 +4,21 @@ from supabase import create_client, Client
 from typing import List, Union, Tuple
 
 
-@st.experimental_singleton
+@st.cache_resource
 def db_init_conn() -> Client:
     url = st.secrets["SUPABASE_PROJECT_URL"]
     key = st.secrets["SUPABASE_API_KEY"]
-    return create_client(url, key)
+
+    # Create client
+    supabase_client = create_client(url, key)
+    # Create session from client
+    supabase_session = supabase_client.auth.sign_in_with_password({'email': st.secrets['SUPABASE_ADMIN_USER'], 'password': st.secrets['SUPABASE_ADMIN_PASS']})
+
+    return supabase_client
 
 
 # @st.experimental_memo(ttl=600, show_spinner=False)
-def db_select(_conn: Client, table_name: str, columns: Union[List[str], str] = '*', query_filter: Tuple[str, str, str] = None) -> Union[pd.DataFrame, bool]:
+def db_select(_conn: Client, table_name: str, columns: Union[List[str], str] = '*', query_filter: Tuple[str, str, str] = None):
     sel_col = ', '.join(columns) if isinstance(columns, list) else columns
 
     try:
@@ -22,29 +28,26 @@ def db_select(_conn: Client, table_name: str, columns: Union[List[str], str] = '
             query = _conn.table(table_name).select(sel_col)
 
         response = query.execute()
-        df = pd.DataFrame.from_records(response.data)
-        return df
+        return response
     except:
         return False
 
 
 # @st.experimental_memo(ttl=600, show_spinner=False)
-def db_insert(_conn: Client, table_name: str, json_data: Union[dict, List[dict]]) -> Union[pd.DataFrame, bool]:
+def db_insert(_conn: Client, table_name: str, json_data: Union[dict, List[dict]]):
+    query = _conn.table(table_name).insert(json_data)
     try:
-        query = _conn.table(table_name).insert(json_data)
         response = query.execute()
-        df = pd.DataFrame.from_records(response.data)
-        return df
     except Exception as e:
-        return e
+        response = e
+    return response.message
 
 
 # @st.experimental_memo(ttl=600, show_spinner=False)
-def db_update(_conn: Client, table_name: str, json_data: Union[dict, list], query_filter: Tuple[str, str, str]) -> Union[pd.DataFrame, bool]:
+def db_update(_conn: Client, table_name: str, json_data: Union[dict, list], query_filter: Tuple[str, str, str]):
     try:
         query = _conn.table(table_name).update(json_data).filter(column=query_filter[0], operator=query_filter[1], criteria=query_filter[2])
         response = query.execute()
-        df = pd.DataFrame.from_records(response.data)
-        return df
+        return response
     except:
         return False
--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,29 +1,66 @@
 import streamlit as st
+from helpers.streamlit import page_recognition, shared_page_config, side_bar_user_login
+from helpers.database import db_init_conn, db_insert
+import os
 
-st.set_page_config(
-    page_title="Home",
-    page_icon="🏠",
-)
 
-st.write("# Visa Manager")
+SCHENGEN_DESC = """
+## Schengen Tourist Visa
+[The Schengen area](https://www.schengenvisainfo.com/schengen-visa-countries-list/) represents a border-free zone 
+between several European countries. In addition, these countries issue a uniform Schengen visa for foreign 
+nationals. To control the comings and goings of millions of people who enter the Schengen area, the 90/180-day rule 
+was established.
 
-main_tab, schengen_tab, usa_tab = st.tabs(['App Desc', 'Schengen', 'USA'])
+What does it mean?
 
-schengen_tab.write("""
-    ## Schengen Tourist Visa
-    [The Schengen area](https://www.schengenvisainfo.com/schengen-visa-countries-list/) represents a border-free zone 
-    between several European countries. In addition, these countries issue a uniform Schengen visa for foreign 
-    nationals. To control the comings and goings of millions of people who enter the Schengen area, the 90/180-day rule 
-    was established.
+The 90/180-day rule states that any foreign national who enters the 
+Schengen zone (any country within the area) can stay for up to 90 days within **any 180 days**. At first glance, it 
+seems a very simple rule, but it’s often misunderstood, and many people overstay it, resulting in them facing 
+penalties. This is why it’s important to know how exactly the 90/180-day rule works. 
+[*www.schengenvisainfo.com*](https://www.schengenvisainfo.com/visa-calculator/)"""
+USA_DESC = """
+## Visa Waiver USA
+something..."""
 
-    What does it mean?
 
-    The 90/180-day rule states that any foreign national who enters the 
-    Schengen zone (any country within the area) can stay for up to 90 days within **any 180 days**. At first glance, it 
-    seems a very simple rule, but it’s often misunderstood, and many people overstay it, resulting in them facing 
-    penalties. This is why it’s important to know how exactly the 90/180-day rule works. 
-    [*www.schengenvisainfo.com*](https://www.schengenvisainfo.com/visa-calculator/)""")
+def main():
+    main_tab, schengen_tab, usa_tab = st.tabs(['App Desc', 'Schengen', 'USA'])
 
-usa_tab.write("""
-    ## Visa Waiver USA
-    something...""")
+    schengen_tab.write(SCHENGEN_DESC)
+    usa_tab.write(USA_DESC)
+
+    with main_tab:
+        st.markdown("## Sing-up")
+        user_agree = st.checkbox("By checking this box, you agree to assume all responsibility for the content you create and share within the app.")
+        c1, c2, c3 = st.columns([2, 2, 1])
+
+        user = c1.text_input(label='Username', placeholder="Username", key="user_signup", autocomplete="email", label_visibility='collapsed', disabled=not user_agree)
+        pswd = c2.text_input(label='Password', placeholder="Password", key="password_signup", type="password", autocomplete="on", label_visibility='collapsed', disabled=not user_agree)
+        if c3.button('Sing-up', type='primary', disabled=not user_agree):
+            insert_response = db_insert(db_conn, 'users', {'username': user, 'password': pswd})
+            if isinstance(insert_response, str) and insert_response == 'duplicate key value violates unique constraint "users_username_key"':
+                st.error('Username already exist, please select another username.')
+            elif isinstance(insert_response, str):
+                st.error('Unknown error, please refresh the page and try again. If this keeps happening please wait a few minutes, the services might be down.')
+
+        st.write('')
+        st.write('')
+        remember_me = st.checkbox("Remember me", disabled=not user_agree)
+        if remember_me:
+            html = """<iframe id="ytplayer" type="text/html" width="560" height="315" src="https://www.youtube.com/embed/KP_XkN2v7OM?autoplay=1&si=ldybZWdUGotYXS9z&amp;controls=0&amp;start=1" allow="autoplay" frameborder="0"></iframe>"""
+            st.components.v1.html(html, width=580, height=335, scrolling=False)
+            st.info("I'm sorry! I haven't implemented sessions yet.")
+
+
+if __name__ == "__main__":
+    # Start with page content
+    shared_page_config(title='Visa Manager',
+                       page_caption='', )
+
+    # Initiate database connection
+    db_conn = db_init_conn()
+    # Sidebar
+    side_bar_user_login(db_conn)
+
+    # Page functionality
+    main()
