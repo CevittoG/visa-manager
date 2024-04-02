4c532ec53d77665618837423703150d03cb75b17
Seba Gutierrez <sebagm123@gmail.com>
2024-04-02 16:59:07 -0500

    user sing-up

--- helpers/database.py
+++ helpers/database.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
 from supabase import create_client, Client
-from typing import List, Union, Tuple
+from typing import List, Union, Tuple, Literal
 
 
+# --------------------------------------------------------------------------- DB CONNECTION
 @st.cache_resource
 def db_init_conn() -> Client:
     url = st.secrets["SUPABASE_PROJECT_URL"]
@@ -17,37 +18,86 @@ def db_init_conn() -> Client:
     return supabase_client
 
 
-# @st.experimental_memo(ttl=600, show_spinner=False)
-def db_select(_conn: Client, table_name: str, columns: Union[List[str], str] = '*', query_filter: Tuple[str, str, str] = None):
+# --------------------------------------------------------------------------- DB QUERIES
+def db_select(_conn: Client, table_name: str, columns: Union[List[str], str] = '*', query_filter: Tuple[str, Literal['eq', 'neq', 'gr', 'gte', 'lt', 'lte', 'like', 'ilike', 'is', 'in', 'contains'], Union[str, List]] = None):
     sel_col = ', '.join(columns) if isinstance(columns, list) else columns
+    if query_filter is not None:
+        query = _conn.table(table_name).select(sel_col).filter(column=query_filter[0], operator=query_filter[1], criteria=query_filter[2])
+    else:
+        query = _conn.table(table_name).select(sel_col)
 
     try:
-        if query_filter is not None:
-            query = _conn.table(table_name).select(sel_col).filter(column=query_filter[0], operator=query_filter[1], criteria=query_filter[2])
-        else:
-            query = _conn.table(table_name).select(sel_col)
-
         response = query.execute()
-        return response
-    except:
-        return False
+        return response.data
+    except Exception as e:
+        return e.message
 
 
-# @st.experimental_memo(ttl=600, show_spinner=False)
 def db_insert(_conn: Client, table_name: str, json_data: Union[dict, List[dict]]):
     query = _conn.table(table_name).insert(json_data)
     try:
         response = query.execute()
+        return response.data
     except Exception as e:
-        response = e
-    return response.message
+        return e.message
 
 
-# @st.experimental_memo(ttl=600, show_spinner=False)
 def db_update(_conn: Client, table_name: str, json_data: Union[dict, list], query_filter: Tuple[str, str, str]):
     try:
         query = _conn.table(table_name).update(json_data).filter(column=query_filter[0], operator=query_filter[1], criteria=query_filter[2])
         response = query.execute()
-        return response
-    except:
+        return response.data
+    except Exception as e:
+        return e.message
+
+
+def db_upsert(_conn: Client, table_name: str, json_data: Union[dict, List[dict]]):
+    query = _conn.table(table_name).insert(json_data)
+    try:
+        response = query.execute()
+        return response.data
+    except Exception as e:
+        return e.message
+
+
+# --------------------------------------------------------------------------- DATA EXTRACTION BY USER ID
+def user_sing_up(_conn: Client, username, password):
+    # Creates User
+    insert_user = db_insert(_conn, 'users', {'username': username, 'password': password})
+    # Check for errors
+    if isinstance(insert_user, str):
+        if insert_user == 'duplicate key value violates unique constraint "users_username_key"':
+            return 'Username already exist, please select another username.'
+        else:
+            if st.secrets['DEBUG']:
+                return insert_user
+            else:
+                return 'Unknown error, please refresh the page and try again. If this keeps happening please wait a few minutes, the services might be down.'
+    else:
+        # Creates TravelHistory
+        insert_th = db_insert(_conn, 'travel_history', {'user_id': insert_user[0]['id']})
+        # Check for errors
+        if isinstance(insert_th, str):
+            if st.secrets['DEBUG']:
+                return insert_th
+            else:
+                return 'Unknown error, please refresh the page and try again. If this keeps happening please wait a few minutes, the services might be down.'
+
+        else:
+            return insert_user[0]['id'], insert_user[0]['username'], insert_th[0]['id']  # user_id, username, th_id
+
+
+def get_user_trips(_conn, th_id):
+    # Get current TravelHistory from db for user
+    travel_history_trips = db_select(_conn, 'travel_history_trips', 'trip_id', ('travel_history_id', 'eq', str(th_id)))
+
+    if not travel_history_trips:
+        # There is no data for travel_history_id
         return False
+    else:
+        # Get current TravelHistory-Trips from db for user
+        trips = db_select(_conn, 'trips', ['country', 'entry_date', 'exit_date'], ('trips', 'in', str(th_id)))  # ToDo
+        return trips
+
+    # bulk upsert
+    # [{'id': idx, 'country': t.country, 'entry_date': t.entry_date, 'exit_date': t.exit_date} for t in self.trips]
--- 🏠_Home.py
+++ 🏠_Home.py
@@ -31,26 +31,28 @@ def main():
     usa_tab.write(USA_DESC)
 
     with main_tab:
-        st.markdown("## Sing-up")
-        user_agree = st.checkbox("By checking this box, you agree to assume all responsibility for the content you create and share within the app.")
-        c1, c2, c3 = st.columns([2, 2, 1])
+        if not st.session_state['LOGGED_IN']:
+            st.markdown("## Sing-up")
+            user_agree = st.checkbox("By checking this box, you agree to assume all responsibility for the content you create and share within the app.")
+            c1, c2, c3 = st.columns([2, 2, 1])
 
-        user = c1.text_input(label='Username', placeholder="Username", key="user_signup", autocomplete="email", label_visibility='collapsed', disabled=not user_agree)
-        pswd = c2.text_input(label='Password', placeholder="Password", key="password_signup", type="password", autocomplete="on", label_visibility='collapsed', disabled=not user_agree)
-        if c3.button('Sing-up', type='primary', disabled=not user_agree):
-            insert_response = db_insert(db_conn, 'users', {'username': user, 'password': pswd})
-            if isinstance(insert_response, str) and insert_response == 'duplicate key value violates unique constraint "users_username_key"':
-                st.error('Username already exist, please select another username.')
-            elif isinstance(insert_response, str):
-                st.error('Unknown error, please refresh the page and try again. If this keeps happening please wait a few minutes, the services might be down.')
-
-        st.write('')
-        st.write('')
-        remember_me = st.checkbox("Remember me", disabled=not user_agree)
-        if remember_me:
-            html = """<iframe id="ytplayer" type="text/html" width="560" height="315" src="https://www.youtube.com/embed/KP_XkN2v7OM?autoplay=1&si=ldybZWdUGotYXS9z&amp;controls=0&amp;start=1" allow="autoplay" frameborder="0"></iframe>"""
-            st.components.v1.html(html, width=580, height=335, scrolling=False)
-            st.info("I'm sorry! I haven't implemented sessions yet.")
+            user = c1.text_input(label='Username', placeholder="Username", key="user_signup", autocomplete="email", label_visibility='collapsed', disabled=not user_agree)
+            pswd = c2.text_input(label='Password', placeholder="Password", key="password_signup", type="password", autocomplete="on", label_visibility='collapsed', disabled=not user_agree)
+            if c3.button('Sing-up', type='primary', disabled=not user_agree):
+                new_user = user_sing_up(db_conn, user, pswd)
+                if isinstance(new_user, str):
+                    st.error(new_user)
+                else:
+                    user_login(*new_user)
+            st.write('')
+            st.write('')
+            remember_me = st.checkbox("Remember me", disabled=not user_agree)
+            if remember_me:
+                html = """<iframe id="ytplayer" type="text/html" width="560" height="315" src="https://www.youtube.com/embed/KP_XkN2v7OM?autoplay=1&si=ldybZWdUGotYXS9z&amp;controls=0&amp;start=1" allow="autoplay" frameborder="0"></iframe>"""
+                st.components.v1.html(html, width=580, height=335, scrolling=False)
+                st.info("I'm sorry! I haven't implemented sessions yet.")
+        else:
+            st.markdown(f"## Welcome {st.session_state.SESSION_USER['username']}!")
 
 
 if __name__ == "__main__":
